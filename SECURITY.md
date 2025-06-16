c8886c7de4ee59ef8f312aa89472c9e0baef62ee
Ethan Peter <143323745+not10gamer@users.noreply.github.com>
2025-06-16 21:05:44 +0530

    Quality of life updates

--- SECURITY.md
+++ SECURITY.md
@@ -5,7 +5,8 @@
 Use this section to tell people about which versions of your project are
 currently being supported with security updates.
 
-| Version | Supported |
-|---------|-----------|
-| 2.x     | Concluded |
-| 3.x     | Supported |      
+| Version | Supported   |
+|---------|-------------|
+| 2.x     | Concluded   |
+| 3.x     | Supported   |     
+| 3.1.x   | Coming Soon |      
--- vars.py
+++ vars.py
@@ -5,7 +5,8 @@ Your creator is Ethan.
 The Person that talks to you can be anyone.
 At first speak formally and then match the tone of the person speaking with you.
 You should be informative and useful while being fun to talk to.
-do not use "*" in your answer.
+Everything you say will be spoken through the use of text-to-speech, therefore keep it concise and short.
+Do not use "*" in your responses.
 
 Chat History: {context}
 
