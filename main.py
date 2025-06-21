80f74f52ed85aeda4d311527064665ed083b55e5
Ethan Peter <143323745+not10gamer@users.noreply.github.com>
2025-06-21 12:52:52 +0530

    Improved workflow for future updates. Preparation for GUI.

--- main.py
+++ main.py
@@ -20,70 +20,75 @@ def record_speech():
     while 1:
         try:
             with speech_recognition.Microphone() as source:
-                recognizer.adjust_for_ambient_noise(source, duration=1)
+                recognizer.adjust_for_ambient_noise(source, duration=.5)
                 print("listening...", end=" ")
-                winsound.Beep(frequency=2000, duration=250)
+                winsound.Beep(frequency=1500, duration=200)
                 audio = recognizer.listen(source, )
                 print("Understanding...")
-                winsound.Beep(frequency=1500, duration=250)
+                winsound.Beep(frequency=1000, duration=200)
                 return recognizer.recognize_google(audio)
 
         except speech_recognition.UnknownValueError as e:
             print("Could not understand audio: {0}".format(e))
-            winsound.Beep(frequency=1000, duration=250)
+            winsound.Beep(frequency=750, duration=200)
         except speech_recognition.RequestError:
             print("Request Error Occurred")
-            winsound.Beep(frequency=1000, duration=250)
+            winsound.Beep(frequency=750, duration=200)
 
 
-context = ""
-start_up = "JARVIS: Hello"
+def main():
+    context = ""
+    start_up = "JARVIS: Hello"
 
-try:
-    file = open("history.txt", "r")
-    context = file.read()
-    file.close()
-except FileNotFoundError:
-    print("No previous history found.")
+    try:
+        file = open("history.txt", "r")
+        context = file.read()
+        file.close()
+    except FileNotFoundError:
+        print("No previous history found.")
 
-context = context + start_up
-print(start_up)
-speak.say(start_up[8:])
-speak.runAndWait()
+    context = context + start_up
+    print(start_up)
+    speak.say(start_up[8:])
+    speak.runAndWait()
 
-while True:
-    if i:
-        user_in = record_speech()
-    else:
-        user_in = input("You: ")
+    while True:
+        if i:
+            user_in = record_speech()
+        else:
+            user_in = input("You: ")
 
-    print(f"You: {user_in}")
-    print("Thinking...")
-    if str(user_in).lower() == "bye":
-        result = chain.invoke({"context": context, "question": "bye"})
-        print("JARVIS: " + result, end="")
-        speak.say(result)
-        speak.runAndWait()
+        print(f"You: {user_in}")
+        print("Thinking...")
+        if str(user_in).lower() == "bye":
+            result = chain.invoke({"context": context, "question": "bye"})
+            print("JARVIS: " + result, end="")
+            speak.say(result)
+            speak.runAndWait()
+            context += f"\nUser: {user_in}\nJarvis: {result}\n"
+
+            file = open("history.txt", "a")
+            file.write(context)
+            file.close()
+            print("Chat history saved.")
+            break
+
+        start = time.time()
+        result = chain.invoke({"context": context, "question": user_in})
+        end = time.time()
+        print(f"Time taken: {end - start}")
+
+        print("JARVIS: " + result)
+
+        if o:
+            if result.startswith("<think>"):
+                speak.say(result.split("</think>")[1])
+            else:
+                speak.say(result)
+            speak.runAndWait()
+
         context += f"\nUser: {user_in}\nJarvis: {result}\n"
 
-        file = open("history.txt", "a")
-        file.write(context)
-        file.close()
-        print("Chat history saved.")
-        break
 
-    start = time.time()
-    result = chain.invoke({"context": context, "question": user_in})
-    end = time.time()
-    print(f"Time taken: {end - start}")
-
-    print("JARVIS: " + result)
-
-    if o:
-        if result.startswith("<think>"):
-            speak.say(result.split("</think>")[1])
-        else:
-            speak.say(result)
-        speak.runAndWait()
-
-    context += f"\nUser: {user_in}\nJarvis: {result}\n"
+if "__main__" == __name__:
+    main()
